dc74b835782de105986fa3e73993a3ba63002a0b
Neil Okamoto <neil.okamoto@gmail.com>
2020-02-07 01:33:00 -0800

    Restructure for multiple OS and multiple geo APIs

--- geolocation.el
+++ geolocation.el
@@ -2,8 +2,8 @@
 
 ;; Author: Neil Okamoto <neil.okamoto+melpa@gmail.com>
 ;; Copyright (C) 2020  Neil Okamoto
-;; Keywords: mapping
-;; Package-Requires: ((osx-plist "2.0.0") (emacs "25.1"))
+;; Keywords: hardware
+;; Package-Requires: ((emacs "25.1"))
 ;; URL: https://github.com/gonewest818/geolocation.el
 ;; Version 0.1.0
 
@@ -24,67 +24,127 @@
 
 ;;; Commentary:
 
-;; This package obtains your current location by submitting the MAC
-;; addresses of nearby wifi access points to third-party geolocation
-;; APIs.  Those services will return your latitude and longitude based
-;; on the recorded positions of those access points.
+;; This package obtains your current location by obtaining the MAC
+;; addresses of nearby wifi access points and then submitting that
+;; list to third-party geolocation APIs.  Those geolocation services
+;; use the known locations of the wifi access points and the relative
+;; strength of each signal to triangulate your latitude and longitude.
 
-;; WARNING: THIS PACKAGE SENDS INFORMATION THAT COORELLATES TO YOUR
-;; LOCATION WILL BE EXPOSED TO THOSE THIRD PARTY GEOLOCATION SERVICES.
-;; IF YOU AREN'T COMFORTABLE WITH THAT FOR ANY REASON THEN DON'T USE
-;; THIS PACKAGE.
+;; WARNING: THIS PACKAGE COLLECTS INFORMATION THAT IMPLIES YOUR
+;; PHYSICAL LOCATION.  THAT DATA WILL BE SENT TO THIRD-PARTY
+;; GEOLOCATION SERVICES WHICH CAN PINPOINT YOUR LOCATION TYPICALLY
+;; WITHIN 100 METERS OR LESS.  WHILE THIS PACKAGE DOES NOT ITSELF
+;; STORE YOUR LOCATION, WE CANNOT GUARANTEE WHAT A GIVEN THIRD-PARTY
+;; SERVICE MAY DO WITH THE DATA IT RECEIVES IN THESE API TRANSACTIONS.
 
 ;;; Code:
 
-(require 'osx-plist)
 (require 'request)
 (require 'json)
 
-(setq geolocation-osx-airport-command
-      (concat  "/System/Library/PrivateFrameworks/"
-               "Apple80211.framework/Versions/Current/Resources/"
-               "airport --scan --xml"
-               "| cat -v"))
+; todo: defcustom
+(setq geolocation-api-vendor :unwiredlabs)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Macintosh
+
+;; Note: macOS provides a core location framework that is used for
+;; Apple's own applications. It is possible to write native code, or
+;; possibly AppleScript code to interact with those frameworks, and
+;; other packages exist to do that. We've chosen geolocation via wifi
+;; because it's more broadly applicable to many operating systems.
+
+(setq geolocation--osx-airport-path
+      (concat "/System/Library/PrivateFrameworks/"
+              "Apple80211.framework/Versions/Current/Resources/"))
+
+(setq geolocation--osx-airport-command
+      (concat geolocation--osx-airport-path
+              "airport --scan | cut -c 34-63 | tail -n +2"))
+
+(defun geolocation--osx-scan-wifi ()
+  "Run the \"airport\" utility and parse the default output."
+  (with-temp-buffer
+    (let ((coding-system-for-read 'utf-8)
+          (result '()))
+      (shell-command geolocation--osx-airport-command t nil)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (push (cl-mapcar (lambda (c k v)
+                           (cons k (if (eq c 'int) (string-to-number v) v)))
+                         '(str int str)
+                         '(bssid signal channel)
+                         (split-string (buffer-substring (point) (point-at-eol))))
+              result)
+        (beginning-of-line 2))
+      result)))
+
+;; macOS "airport" utilty also outputs a property list in XML format
+;; which in principle should be more reliable to parse, and contains
+;; more interesting information than the human readable output.
+;; However as of 2/1/2020 the "airport" utility truncates output when
+;; devices advertise themselves with names containing unprintable
+;; characters. The truncation bug cannot be worked around by the
+;; caller.  Hide this implementation until/unless the underlying bug
+;; is fixed.
+
+;; (require 'osx-plist)
+
+;; (setq geolocation--osx-airport-command-xml
+;;       (concat geolocation--osx-airport-path
+;;               "airport --scan --xml"))
+
+;; (defun geolocation--osx-parse-access-point (ap)
+;;   "Select relevant fields from the access point record AP.
+
+;; The result is an alist containing at least the following keys:
+;;   `bssid'   : BSSID / MAC address of the access point
+;;   `signal'  : signal strength or RSSI from that access point
+;;   `channel' : channel on which that access point is operating"
+;;   (list (cons 'bssid (gethash "BSSID" ap))
+;;         (cons 'signal (gethash "RSSI" ap))
+;;         (cons 'channel (gethash "CHANNEL" ap))))
+
+;; (defun geolocation--osx-call-airport-xml ()
+;;   "Run the \"airport\" utility and parse the xml output."
+;;   (with-temp-buffer
+;;     (let ((coding-system-for-read 'utf-8))
+;;       (shell-command geolocation--osx-airport-command-xml t nil))
+;;     (mapcar #'geolocation--osx-parse-access-point (osx-plist-parse-buffer))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Windows
+
+(defun geolocation--windows-scan-wifi ()
+  "Wifi scanning on Windows not yet implemented."
+  (error "Wifi scanning on Windows not yet implemented"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Linux
+
+(defun geolocation--linux-scan-wifi ()
+  "Wifi scanning on Linux not yet implemented."
+  (error "Wifi scanning on Linux not yet implemented"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Google geolocation api
+
+(setq geolocation-api-google-url nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Unwired Labs geolocation api
 
 (setq geolocation-api-unwiredlabs-url
+;;    "https://us1.unwiredlabs.com/v2/process.php" ; Virginia
+;;    "https://us2.unwiredlabs.com/v2/process.php" ; San Francisco
+;;    "https://eu1.unwiredlabs.com/v2/process.php" ; France
+;;    "https://ap1.unwiredlabs.com/v2/process.php" ; Singapore
       "https://us2.unwiredlabs.com/v2/process.php")
 
 (setq geolocation-api-unwiredlabs-token
       (if-let ((pw (car (auth-source-search :host "unwiredlabs.com"))))
           (funcall (plist-get pw :secret))))
-
-(setq geolocation-api-google-url nil)
-
-(setq geolocation-api-vendor :unwiredlabs)
-
-(defun geolocation--osx-call-airport ()
-  "Run the \"airport\" utility and get xml output."
-  (with-temp-buffer
-    ;; DEBUGING: with-current-buffer (get-buffer-create "airport.plist")
-    (let ((coding-system-for-read 'utf-8))
-      (shell-command geolocation-osx-airport-command t nil))
-    (osx-plist-parse-buffer)))
-
-(defun geolocation--osx-parse-access-point (ap)
-  "Select relevant fields from the access point record AP.
-
-The result is an alist containing at least the following keys:
-  `bssid'   : BSSID / MAC address of the  access point
-  `signal'  : signal strength or RSSI from that access point
-  `channel' : channel on which that access point is operating"
-  (list (cons 'bssid (gethash "BSSID" ap))
-        (cons 'signal (gethash "RSSI" ap))
-        (cons 'channel (gethash "CHANNEL" ap))))
-
-(defun geolocation-get-wifi-access-points ()
-  "Return a list of nearby wifi access points.
-
-The result is sorted by signal strength with the strongest first."
-  (if-let ((aps (geolocation--osx-call-airport)))
-      (let ((parsed (mapcar #'geolocation--osx-parse-access-point aps)))
-        (sort parsed (lambda (x y)
-                       (> (alist-get 'signal x)
-                          (alist-get 'signal y)))))))
 
 (defun geolocation--call-unwiredlabs-api (wifi)
   "Invoke the Unwiredlabs REST API with WIFI data and get position."
@@ -94,10 +154,32 @@ The result is sorted by signal strength with the strongest first."
            :data (json-encode `(("token" . ,geolocation-api-unwiredlabs-token)
                                 ("wifi" . ,wifi)))
            :parser #'json-read
-           :sync t
+           :sync t                      ; todo: make this an async call
            :timeout 15)))
     (when (= 200 (request-response-status-code response))
       (request-response-data response))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Public API
+
+(defun geolocation-scan-wifi ()
+  "Return a list of nearby wifi access points.
+The result is sorted by signal strength with the strongest first.
+
+Each item in the list is itself an alist with the following keys:
+  `bssid'   : mac address of the access point
+  `signal'  : relative signal strength or rssi
+  `channel' : broadcast channel"
+  (if-let ((aps (cond
+                 ((string-equal system-type "darwin")
+                  (geolocation--osx-scan-wifi))
+                 ((string-equal system-type "windows-nt")
+                  (geolocation--windows-scan-wifi))
+                 ((string-equal system-type "gnu/linux")
+                  (geolocation--linux-scan-wifi)))))
+      (sort aps (lambda (x y)
+                  (> (alist-get 'signal x)
+                     (alist-get 'signal y))))))
 
 (defun geolocation-get-position ()
   "Return our current latitude and longitude.
@@ -109,8 +191,9 @@ The reply is an alist with at least the following keys:
 
 Other keys may be included in the result, but are not guaranteed
 to be supported going forward."
-  (if-let ((wifi (geolocation-get-wifi-access-points)))
-      (geolocation--call-unwiredlabs-api wifi)))
+  (if-let ((wifi (geolocation-scan-wifi)))
+      (cond ((eq :unwiredlabs geolocation-api-vendor)
+             (geolocation--call-unwiredlabs-api wifi)))))
 
 (provide 'geolocation)
 ;;; geolocation.el ends here
